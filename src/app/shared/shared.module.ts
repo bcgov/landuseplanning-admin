920c8497f6b8cb91a7f70d4c7aec7a8b827a2e90
Severin Beauvais <severin.beauvais@gmail.com>
2018-02-25 14:00:30 -0800

    PRC-131: ported file-upload component from UI

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 // import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+// import { MatProgressBarModule } from '@angular/material';
+
 import { OrderByPipe } from '../pipes/order-by.pipe';
 import { NewlinesPipe } from '../pipes/newlines.pipe';
 
+import { FileUploadComponent } from 'app/file-upload/file-upload.component';
+
 @NgModule({
   imports: [
-    // CommonModule
+    // CommonModule,
+    BrowserModule,
+    // MatProgressBarModule
   ],
   declarations: [
     OrderByPipe,
-    NewlinesPipe
+    NewlinesPipe,
+    FileUploadComponent
   ],
   exports: [
     OrderByPipe,
-    NewlinesPipe
+    NewlinesPipe,
+    FileUploadComponent
   ]
 })
 
