ad942218bf0ea08092e5ab2b6773240a1ba358ac
Jeremy Dunsdon <twenty15creative@gmail.com>
2018-10-24 15:04:12 -0700

    [PRC-763] Implement Not Authorized Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
+import { NotAuthorizedComponent } from './not-authorized/not-authorized.component';
 import { SearchComponent } from './search/search.component';
 import { HomeComponent } from './home/home.component';
 import { AdministrationComponent } from 'app/administration/administration.component';
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'administration/users',
     component: UsersComponent
+  },
+  {
+    path: 'not-authorized',
+    component: NotAuthorizedComponent
   },
   {
     path: 'search',
