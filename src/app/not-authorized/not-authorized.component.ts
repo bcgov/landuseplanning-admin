7f057bf9bcc2dba36b634ab70459113de1a3c52e
Mark Lisé <mark@digitalspace.ca>
2018-10-30 11:25:14 -0700

    PRC-501: Updating not-authorized to handle logged out state.

--- src/app/not-authorized/not-authorized.component.ts
+++ src/app/not-authorized/not-authorized.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Subject } from 'rxjs/Subject';
+import { ApiService } from 'app/services/api';
 
 @Component({
   selector: 'app-not-authorized',
@@ -6,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-authorized.component.scss']
 })
 export class NotAuthorizedComponent implements OnInit {
+  private ngUnsubscribe: Subject<boolean> = new Subject<boolean>();
+  public loggedout = false;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private api: ApiService
+  ) { }
 
   ngOnInit() {
+    this.route.queryParamMap
+    .takeUntil(this.ngUnsubscribe)
+    .subscribe(paramMap => {
+      this.loggedout = paramMap.get('loggedout') === 'true';
+    });
+  }
+
+  login() {
+    // Either go to the local login, or attempt IDIR auth
+    this.api.ensureLoggedIn();
+    this.router.navigate(['search']);
   }
 }
