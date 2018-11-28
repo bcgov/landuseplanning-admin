e89d97d39572b248703b9337a5bae78f4b1f0462
Mark Lisé <mark@digitalspace.ca>
2018-11-28 14:48:19 -0800

    PRC-865: Tweaks to IDIR display name/fix for token in localhost mode.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -52,7 +52,7 @@ export class HeaderComponent implements OnInit {
         // console.log("token:", token);
         const jwt = new JwtUtil().decodeToken(token);
         console.log('jwt:', jwt);
-        this.welcomeMsg = jwt ? ('Hello ' + jwt.display_name) : 'Login';
+        this.welcomeMsg = jwt ? ('Hello ' + jwt.displayName) : 'Login';
         // console.log("this:", this.welcomeMsg);
         this.jwt = jwt;
       } else {
