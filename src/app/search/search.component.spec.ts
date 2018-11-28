fbef1244782ee401bb9ab43f554ffbe66e87cf21
Andrew Whillans <Andrew.Whillans@gov.bc.ca>
2018-11-28 15:49:20 -0800

    Get a few more component specs passing

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatSnackBar } from '@angular/material';
 import { ApplicationService } from 'app/services/application.service';
