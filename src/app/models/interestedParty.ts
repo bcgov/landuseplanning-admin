7def243ea1222bb6a40ce8e1dbcd7e828a1b55c7
Mark Lisé <mark@digitalspace.ca>
2018-12-17 13:33:10 -0800

    PRC-832: Change admin UI to reference new ttlsapi endpoints.

---
+++ src/app/models/interestedParty.ts
@@ -0,0 +1,15 @@
+export class InterestedParty {
+  divisionBranch: string;
+  interestedPartyType: string;
+  legalName: string;
+  firstName: string;
+  lastName: string;
+
+  constructor(obj?: any) {
+    this.divisionBranch       = obj && obj.divisionBranch       || null;
+    this.interestedPartyType  = obj && obj.interestedPartyType  || null;
+    this.legalName            = obj && obj.legalName            || null;
+    this.firstName            = obj && obj.firstName            || null;
+    this.lastName             = obj && obj.lastName             || null;
+  }
+}
--- src/app/models/search.ts
+++ src/app/models/search.ts
@@ -3,6 +3,7 @@ import * as _ from 'lodash';
 
 import { Feature } from './feature';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { InterestedParty } from './interestedParty';
 
 export class SearchResults {
   _id: string;
@@ -15,6 +16,22 @@ export class SearchResults {
   features: Array<Feature> = [];
   sidsFound: Array<string> = [];
 
+  // New Data
+  CROWN_LANDS_FILE: string;
+  DISPOSITION_TRANSACTION_SID: string;
+  RESPONSIBLE_BUSINESS_UNIT: string;
+  TENURE_LOCATION: string;
+  TENURE_PURPOSE: string;
+  TENURE_STAGE: string;
+  TENURE_STATUS: string;
+  TENURE_SUBPURPOSE: string;
+  TENURE_SUBTYPE: string;
+  TENURE_TYPE: string;
+  areaHectares: number;
+  centroid: Array<number> = [];
+  interestedParties: Array<InterestedParty> = [];
+  parcels: Array<Feature> = [];
+
   constructor(search?: any, hostname?: any) {
     this._id           = search && search._id           || null;
     this.totalFeatures = search && search.totalFeatures || 0;
@@ -24,10 +41,32 @@ export class SearchResults {
     this.status        = search && search.status        || null;
     this.hostname      = hostname;
 
+    this.CROWN_LANDS_FILE             = search && search.CROWN_LANDS_FILE || null;
+    this.DISPOSITION_TRANSACTION_SID  = search && search.DISPOSITION_TRANSACTION_SID || null;
+    this.RESPONSIBLE_BUSINESS_UNIT    = search && search.RESPONSIBLE_BUSINESS_UNIT || null;
+    this.TENURE_LOCATION              = search && search.TENURE_LOCATION || null;
+    this.TENURE_PURPOSE               = search && search.TENURE_PURPOSE || null;
+    this.TENURE_STAGE                 = search && search.TENURE_STAGE || null;
+    this.TENURE_STATUS                = search && search.TENURE_STATUS || null;
+    this.TENURE_SUBPURPOSE            = search && search.TENURE_SUBPURPOSE || null;
+    this.TENURE_SUBTYPE               = search && search.TENURE_SUBTYPE || null;
+    this.TENURE_TYPE                  = search && search.TENURE_TYPE || null;
+    this.TENURE_TYPE                  = search && search.TENURE_TYPE || null;
+    this.areaHectares                 = search && search.areaHectares || null;
+    this.centroid                     = search && search.centroid || null;
+    this.parcels                      = search && search.parcels || null;
+
     // copy features
     if (search && search.features) {
       for (const feature of search.features) {
         this.features.push(feature);
+      }
+    }
+
+    // copy interestedParties
+    if (search && search.interestedParties) {
+      for (const party of search.interestedParties) {
+        this.interestedParties.push(party);
       }
     }
 
