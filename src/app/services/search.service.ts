123f474681426f114bea287d3281c4c0af248fb9
Alex <awintschel@gmail.com>
2022-05-25 13:29:21 -0700

    DESENG-135 Update moment npm dependency

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -4,7 +4,6 @@ import { of } from 'rxjs';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import * as _ from 'lodash';
-import * as moment from 'moment';
 
 import { ApiService } from './api';
 import { SearchResults } from 'app/models/search';
