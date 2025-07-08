b11865750d58656e7d8bc53973f0fe75c287655b
Alexander Wintschel <alexander.wintschel@gov.bc.ca>
2025-07-08 16:32:43 -0700

    DESENG-869 Remove unecessary isEditing check

--- src/app/projects/add-edit-project/add-edit-project.component.ts
+++ src/app/projects/add-edit-project/add-edit-project.component.ts
@@ -132,7 +132,7 @@ export class AddEditProjectComponent implements OnInit, AfterViewInit, OnDestroy
     .takeUntil(this.ngUnsubscribe)
     .subscribe((data: { project: Project }) => {
       if (data.project) {
-        this.isEditing = Object.keys(data).length === 0 && data.constructor === Object ? false : true;
+        this.isEditing = data.constructor === Object ? true : false;
         
         /**
          * When a user selects a project lead(and is taken to a new window),
